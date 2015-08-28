61096c5a746ca3a64c7f2e86cee10ff14bd6a6b7
f3r10 <frledesma@outlook.com>
2015-08-28 18:14:55 -0500

    First test

---
+++ src/main/java/Game.java
@@ -0,0 +1,11 @@
+package src.main.java;
+public class Game{
+
+	private int score = 0;
+	public  int roll(int pins){
+		return score += pins;
+	} 
+	public int  score(){
+		return score;
+	}
+}
---
+++ src/test/java/TestGame.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import src.main.java.Game;
+
+
+public class TestGame{
+
+	@Test
+	public void testPinsWereNotHittedInAllFrames(){
+		Game game  = new Game ();
+		for (int i = 0; i<20; i++){
+			game.roll(0);
+		}
+		assertThat(0, is(game.score()));
+	}
+}
